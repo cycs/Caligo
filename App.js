81c657585ea4bd1921ac662ad29d846a36954fe7
Christopher Saenen <saenen.chris@outlook.com>
2019-03-17 21:39:11 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,28 +1,22 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View, Button, Image} from 'react-native';
-import Mapbox from '@mapbox/react-native-mapbox-gl';
 import {name as appName} from './app.json';
-import communesJSON from './Communes-belgique.json';
-import * as turf from '@turf/turf';
-import update from 'immutability-helper';
-import { point } from '@turf/helpers';
-import PositionButton from './src/components/PositionButton';
-import TabsNavigator from './src/components/tabsNavigator';
-import markerMyPosition from './src/img/marker.png';
-import Map from './src/components/Map';
-import Completion from './src/components/Completion';
-import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
+import Tab from './src/components/tabsNavigator';
+import { connect } from 'react-redux';
+import store from './src/components/Store';
+import { Provider } from 'react-redux';
 
 
 class App extends React.Component {
     render() {
-      return <Map/>
+        // console.log('THIS DOT PROPS', this.props)
+      return (
+    <Provider store={store}>
+        <Tab/>
+    </Provider>
+      )
     }
   }
 
-  const TabNavigator = createBottomTabNavigator({
-    Home: Map,
-    Settings: Completion,
-  });
-  
-  export default createAppContainer(TabNavigator);
+
+export default App;
